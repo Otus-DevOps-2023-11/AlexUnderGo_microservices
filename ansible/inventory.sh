631eef3360837acfe7175d8450aa55283838cb10
AlexUndrGo <alex.undr.b@gmail.com>
2024-04-15 18:32:54 +0300

    gitlab-ci-1

--- ansible/inventory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-terraform_outputs=`cd ../terraform; terraform output`
-gitlab_ip=`echo "$terraform_outputs" | grep external_ip_address_gitlab | awk '{print $3}'`
-output_ip="{\"_meta\":{\"hostvars\":{}},\"vm-gitlab\":{\"hosts\":[$gitlab_ip]}}"
-echo $output_ip
